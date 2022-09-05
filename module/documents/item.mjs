05331e3339aa7b564f7a40653255386cd2a55cb5
Tenyryas <tenyryas@gmail.com>
2022-09-05 17:42:47 +0200

    v10 data to system fix

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -49,7 +49,7 @@ export class LHTrpgItem extends Item {
     // If present, return the actor's roll data.
     if (!this.actor) return null;
     const rollData = this.actor.getRollData();
-    rollData.item = foundry.utils.deepClone(this.data.data);
+    rollData.item = foundry.utils.deepClone(this.system);
 
     return rollData;
   }
